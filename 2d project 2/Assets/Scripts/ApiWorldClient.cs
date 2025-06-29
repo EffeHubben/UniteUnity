842661f64a33696f0d4d9108123ab6f92b8b8958
EffeHubben <120639863+EffeHubben@users.noreply.github.com>
2025-06-30 01:14:48 +0200

    Werkend

--- 2d project 2/Assets/Scripts/ApiWorldClient.cs
+++ 2d project 2/Assets/Scripts/ApiWorldClient.cs
@@ -103,7 +103,7 @@ public class ApiWorldClient : MonoBehaviour
                 maxHeight = 200
             };
             string jsonData = JsonUtility.ToJson(registerDto);
-            var response = await PerformApiCall("https://localhost:7015/wereldbouwer", "POST", jsonData, SessionData.token);
+            var response = await PerformApiCall("https://avansict2199044.azurewebsites.net/wereldbouwer", "POST", jsonData, SessionData.token);
             Debug.Log(response);
         } else
         {
@@ -116,7 +116,7 @@ public class ApiWorldClient : MonoBehaviour
 
     private async Task<bool> WorldNameExists(string worldName, string ownerUserId)
     {
-        string url = $"https://localhost:7015/wereldbouwer/getwereld/{ownerUserId}";
+        string url = $"https://avansict2199044.azurewebsites.net/wereldbouwer/getwereld/{ownerUserId}";
         var response = await PerformApiCall(url, "GET", null, SessionData.token);
 
         if (response == null)
@@ -155,7 +155,7 @@ public class ApiWorldClient : MonoBehaviour
             {
                 Destroy(child.gameObject);
             }
-            string url = $"https://localhost:7015/wereldbouwer/getwereld/{SessionData.ownerUserId}";
+            string url = $"https://avansict2199044.azurewebsites.net/wereldbouwer/getwereld/{SessionData.ownerUserId}";
             var response = await PerformApiCall(url, "GET", null, SessionData.token);
 
             if (response == null)
@@ -226,7 +226,7 @@ public class ApiWorldClient : MonoBehaviour
         if (SessionData.token != null)
         {
             await DeleteWorldObjectsFromLoad(worldId);
-            string url = $"https://localhost:7015/wereldbouwer/{worldId}";
+            string url = $"https://avansict2199044.azurewebsites.net/wereldbouwer/{worldId}";
             var response = await PerformApiCall(url, "DELETE", null, SessionData.token);
 
             if (response != null)
@@ -248,7 +248,7 @@ public class ApiWorldClient : MonoBehaviour
 
     public async Task DeleteWorldObjectsFromLoad(string worldId)
     {
-        string url = $"https://avansict2228256.azurewebsites.net/Object2D/environment/{worldId}";
+        string url = $"https://avansict2199044.azurewebsites.net/Object2D/environment/{worldId}";
         string response = await PerformApiCall(url, "DELETE", null, SessionData.token);
 
         Debug.Log(response);
